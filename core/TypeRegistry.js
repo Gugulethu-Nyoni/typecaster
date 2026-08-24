da3522c94972f1325ce4a470d9f17cd9518fe52e
Gugu <gugunnn@gmail.com>
2026-08-24 19:26:25 +0200

    feat: implement TypeRegistry

---
+++ core/TypeRegistry.js
@@ -0,0 +1,44 @@
+class TypeRegistry {
+    constructor() {
+        this.types = new Map();
+    }
+
+    register(name, definition) {
+        if (!name) {
+            throw new Error('Type name is required');
+        }
+
+        if (!definition || typeof definition !== 'object') {
+            throw new Error(`Type definition for "${name}" must be an object`);
+        }
+
+        this.types.set(name, {
+            name,
+            ...definition
+        });
+
+        return this.types.get(name);
+    }
+
+    resolve(name) {
+        return this.types.get(name);
+    }
+
+    has(name) {
+        return this.types.has(name);
+    }
+
+    getAll() {
+        return Array.from(this.types.values());
+    }
+
+    remove(name) {
+        return this.types.delete(name);
+    }
+
+    clear() {
+        this.types.clear();
+    }
+}
+
+module.exports = TypeRegistry;
